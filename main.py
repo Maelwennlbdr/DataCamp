db23575e887029183c8ff42ce95c8fdd3c613cde
Maelwenn <maelwenn@labidurie.fr>
2024-10-16 18:22:59 +0200

    commentaires

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import streamlit as st
 from PIL import Image
 import base64
 api_key = "6a782f6af3be4da4afb16a74a23e316f"
-# Activer la mise en page large
+
 st.set_page_config(layout="wide")
 
 def get_base64_of_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-# Ajouter un style CSS pour changer la couleur de fond, la couleur du texte et le style des images
+
 st.markdown("""
     <style>
     .main {
@@ -84,15 +84,15 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# Appliquer la classe de style principale
+
 st.markdown('<div class="main">', unsafe_allow_html=True)
 
-# Charger l'image
+
 image_path = "images/photo1.png"
 image_base64 = get_base64_of_image(image_path)
 
-# CSS pour positionner le texte correctement au centre de l'image avec une police plus grande
-# Section d'en-tête avec l'image et le titre
+
+# Part 1 - image
 st.markdown(
     f"""
     <style>
@@ -144,7 +144,7 @@ st.markdown(
     """, unsafe_allow_html=True
 )
 
-# Section de la table des matières
+# Part 2 - Table of Contents
 st.markdown("<h1 class='markdown-text' style='text-align: center'>Table of Contents</h1>", unsafe_allow_html=True)
 col1, col2, col3 = st.columns(3)
 
@@ -173,25 +173,17 @@ with col3:
     """, unsafe_allow_html=True)
 
 st.markdown("\n\n\n")
+# Part 3 - About
 
-# Charger l'image
 image_path = "images/about.png"
 image_base64 = get_base64_of_image(image_path)
 
-col21, col22 = st.columns(2)
-with col21:
-    # Centrer l'image avec un style CSS
-    st.markdown(f"""
-        <div style="text-align: center;">
-            <img src="data:image/png;base64,{image_base64}" style='border-radius: 25px; max-width: 100%; height: auto;'>
-        </div>
-    """, unsafe_allow_html=True)
-
-with col22:
-    st.markdown("""
-        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100%;">
-            <h2 class='markdown-text'>About PlateMate</h2>
-            <span class='markdown-text' style='text-align: center; margin: 20px;'>
+st.markdown(f"""
+    <div style="display: flex; align-items: center; justify-content: center;">
+        <img src="data:image/png;base64,{image_base64}" style='border-radius: 25px; max-width: 100%; height: auto; margin-right: 50px; margin-left: 100px;'>
+        <div>
+            <h1 class='markdown-text' style="text-align: center;">About PlateMate</h1>
+            <span class='markdown-text' style='margin: 20px; font-size: 20px;'>
                 The goal of PlateMate is to simplify daily meal management by providing 
                 a visual, easy-to-use meal planner for healthy eating, along with precise 
                 calculations of calories for each meal.
@@ -201,51 +193,42 @@ with col22:
                 cater to all kinds of dietary requirements.
             </span>
         </div>
-    """, unsafe_allow_html=True)
+    </div>
+""", unsafe_allow_html=True)
 
 
 st.markdown("\n\n")
 
-# Partie 4
+# Part 4 - Recipes
 
-# Charger l'image
 image = Image.open("images/photo2.jpeg")
 
-# Création de deux colonnes pour la mise en page
-col1, col2 = st.columns([1, 1])  # Deux colonnes de même taille
+st.markdown("<div id='recipes'></div>", unsafe_allow_html=True)
+st.markdown(f"""
+    <div style="display: flex; align-items: center; justify-content: center;">
+        <div style="text-align: center; margin-right: 20px;">
+            <h1 style='font-size: 60px; color: #F2E8CF; margin-top: 100px;'>Choose the perfect recipes</h1>
+            <h2 style='font-size: 60px; color: #F2E8CF; margin-top: 20px;'>for you with the numerous filters!</h2>
+        </div>
+        <img src="data:image/jpeg;base64,{get_base64_of_image('images/photo2.jpeg')}" style='border-radius: 25px; max-width: 50%; height: auto;'>
+    </div>
+""", unsafe_allow_html=True)
 
-with col1:
-    st.markdown(
-        "<h1 style='text-align: center; font-size: 60px; color: #F2E8CF; margin-top: 100px;'>Choose the perfect recipes</h1>",
-        unsafe_allow_html=True)
-    st.markdown(
-        "<h2 style='text-align: center; font-size: 60px; color: #F2E8CF; margin-top: 20px;'>for you with the numerous filters!</h2>",
-        unsafe_allow_html=True)
-with col2:
-    # Afficher l'image dans la première colonne
-    st.image(image, use_column_width=True)
 
-# Partie 5
 import requests
 
-# Clé d'API pour Spoonacular (à remplacer par ta propre clé)
-
-
-# Interface de l'application
-st.markdown("<div id='recipes'></div>", unsafe_allow_html=True)
 st.markdown("<h1 style='color: #F2E8CF'>Recipes</h1>", unsafe_allow_html=True)
 
-# Régimes alimentaires disponibles dans l'API Spoonacular
+# Diet in Spoonacular
 diet_options = [
-    "None",  # Option pour indiquer qu'aucun régime alimentaire n'est sélectionné
+    "None",
     "Gluten Free", "Ketogenic", "Vegetarian", "Lacto-Vegetarian", "Ovo-Vegetarian",
     "Vegan", "Pescetarian", "Paleo", "Primal", "Whole30"
 ]
 
-# Nouvelles tranches de calories
-calorie_options = ["Moins de 200", "200-500", "500-700", "Plus de 700"]
+# Calories slice
+calorie_options = ["Less than 200", "200-500", "500-700", "More than 700"]
 
-# Organiser les filtres en menus déroulants
 with st.expander("Select a diet (optional)"):
     selected_diet = st.selectbox("", diet_options)
 
@@ -253,10 +236,9 @@ with st.expander("Select a calorie range"):
     selected_calories = st.selectbox("", calorie_options)
 
 
-# Fonction pour appeler l'API Spoonacular et obtenir des recettes en fonction du régime et des calories
 def get_recipes_for_diet_and_calories(diet, calories):
     # Définir la tranche de calories
-    if calories == "Moins de 200":
+    if calories == "Less than 200":
         min_cal = 0
         max_cal = 200
     elif calories == "200-500":
@@ -265,11 +247,10 @@ def get_recipes_for_diet_and_calories(diet, calories):
     elif calories == "500-700":
         min_cal = 500
         max_cal = 700
-    else:  # "Plus de 700"
+    else:  # "More than 700"
         min_cal = 700
-        max_cal = 10000  # Mettre une grande valeur pour inclure toutes les recettes au-dessus de 700
+        max_cal = 10000
 
-    # Si aucun régime n'est sélectionné, ne pas inclure le paramètre diet
     if diet == "None":
         url = f"https://api.spoonacular.com/recipes/complexSearch?minCalories={min_cal}&maxCalories={max_cal}&number=6&apiKey={api_key}"
     else:
@@ -279,24 +260,21 @@ def get_recipes_for_diet_and_calories(diet, calories):
     data = response.json()
     return data['results']
 
-# Fonction pour obtenir les détails de la recette
 def get_recipe_details(recipe_id):
     url = f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={api_key}"
     response = requests.get(url)
     data = response.json()
     return data
 
-# Si un régime ou une tranche de calories sont sélectionnés, afficher les recettes correspondantes
 if selected_calories:
     st.markdown(f"<h3 style='color: #F2E8CF'>Recipes with {selected_calories} calories</h3>", unsafe_allow_html=True)
     recipes = get_recipes_for_diet_and_calories(selected_diet.lower(), selected_calories)
 
-    # Créer autant de colonnes que de recettes à afficher (ici 3 pour la mise en page)
     cols = st.columns(3)
 
     for idx, recipe in enumerate(recipes):
         recipe_id = recipe['id']
-        col_index = idx % 3  # Détermine la colonne à utiliser
+        col_index = idx % 3
 
         with cols[col_index]:
             st.image(recipe['image'], use_column_width=True)
@@ -307,21 +285,17 @@ if selected_calories:
                 st.write(f"**Servings**: {details['servings']}")
                 st.write(f"**Ready in**: {details['readyInMinutes']} minutes")
 
-                # Afficher les instructions, en s'assurant qu'elles sont formatées correctement
                 instructions = details['instructions'] if details['instructions'] else "No instructions available."
 
-                # Si les instructions sont en format HTML, utilisez st.markdown
                 st.markdown(instructions, unsafe_allow_html=True)
 
 
-# Partie 6
+# Part 5 - Meal Planner
 import random
 
-# Titre principal
 st.markdown("<div id='meal-planner'></div>", unsafe_allow_html=True)
 st.markdown(f"<h1 class='recipe-title' style='text-align: left;'>Meal Planning for the Week</h1>", unsafe_allow_html=True)
 
-# Sélection du régime alimentaire via un menu déroulant où l'on peut choisir un seul régime
 st.markdown("<p class='recipe-title' style='text-align: left;'>Select a Diet (optional)</p>", unsafe_allow_html=True)
 selected_diet = st.selectbox(
     "",
@@ -329,45 +303,34 @@ selected_diet = st.selectbox(
 )
 
 
-# Liste des jours de la semaine
 st.markdown("<p class='recipe-title' style='text-align: left;'>Select a Day</p>", unsafe_allow_html=True)
 
 days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 selected_day = st.selectbox("", days_of_week)
 
 
-# Fonction pour appeler l'API Spoonacular avec le régime sélectionné
 def get_meal_plan(diet):
-    # Construire l'URL pour l'API
     url = f"https://api.spoonacular.com/mealplanner/generate?apiKey={api_key}&timeFrame=week"
 
-    # Ajouter le régime à la requête s'il est sélectionné
     if diet and diet != "None":
         url += f"&diet={diet.lower()}"
 
-    # Ajouter un paramètre aléatoire pour que l'API retourne des résultats variés
     url += f"&random={random.randint(1, 1000)}"
 
-    # Faire la requête à l'API
     response = requests.get(url)
     return response.json()
 
 
-# Si un régime est sélectionné, obtenir le plan de repas correspondant
 if selected_diet:
     meal_plan_data = get_meal_plan(selected_diet)
 
-    # Vérification des résultats
     if meal_plan_data and 'week' in meal_plan_data:
-        # Obtenir les repas pour le jour sélectionné
         day_meals = meal_plan_data['week'][selected_day.lower()]['meals']
 
         st.markdown(f"<h4 class='recipe-title' style='text-align:left'>Meal Plan for {selected_day}</h4>", unsafe_allow_html=True)
 
-        # Utiliser trois colonnes pour afficher le petit-déjeuner, le déjeuner et le dîner
         col1, col2, col3 = st.columns(3)
 
-        # Afficher le petit-déjeuner dans la première colonne
         with col1:
             st.markdown(f"<h3 class='recipe-title'>Breakfast</h3>", unsafe_allow_html=True)
             meal = day_meals[0]
@@ -386,7 +349,6 @@ if selected_diet:
                             </p>
                             """, unsafe_allow_html=True)
 
-        # Afficher le déjeuner dans la deuxième colonne
         with col2:
             st.markdown(f"<h3 class='recipe-title'>Lunch</h3>", unsafe_allow_html=True)
             meal = day_meals[1]
@@ -405,7 +367,6 @@ if selected_diet:
                 </p>
                 """, unsafe_allow_html=True)
 
-        # Afficher le dîner dans la troisième colonne
         with col3:
             st.markdown(f"<h3 class='recipe-title'>Dinner</h3>", unsafe_allow_html=True)
             meal = day_meals[2]
@@ -430,11 +391,10 @@ if selected_diet:
 else:
     st.write("Please select a diet to generate a meal plan.")
 
-# Titre principal
+# Part 6 - Foodselection
 st.markdown("<div id='food-selection'></div>", unsafe_allow_html=True)
 st.markdown("<h1 style='color: #F2E8CF' id='food-selection'>Food-selection</h1>", unsafe_allow_html=True)
 
-# Dictionnaire pour stocker les sélections
 selected_foods = {
     "Vegetables": [],
     "Protein (Meat & Plant-Based)": [],
@@ -449,7 +409,6 @@ selected_foods = {
     "Diets": []
 }
 
-# Utiliser des menus déroulants pour organiser les filtres
 with st.expander("Vegetables"):
     for i, vegetable in enumerate(["Carrot", "Broccoli", "Spinach", "Kale", "Bell Peppers", "Zucchini"]):
         if st.checkbox(vegetable, key=f'vegetable_{i}'):
@@ -506,17 +465,13 @@ with st.expander("Diets"):
             selected_foods["Diets"].append(diet)
 
 
-# Fonction pour appeler l'API Spoonacular en fonction des aliments sélectionnés
 def get_recipes_based_on_foods(selected_foods):
-    # Combine toutes les sélections d'ingrédients dans une chaîne de caractères
     ingredients = []
     for category, items in selected_foods.items():
         ingredients.extend(items)
 
-    # Créer une chaîne avec les ingrédients séparés par des virgules
     ingredient_query = ','.join(ingredients)
 
-    # Faire une requête à l'API pour obtenir les recettes basées sur les ingrédients
     if ingredient_query:
         url = f"https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredient_query}&number=6&apiKey={api_key}"
         response = requests.get(url)
@@ -526,12 +481,10 @@ def get_recipes_based_on_foods(selected_foods):
         return []
 
 
-# Si l'utilisateur a sélectionné des aliments, appeler l'API et afficher les résultats
-if any(selected_foods.values()):  # Vérifier si des aliments ont été sélectionnés
+if any(selected_foods.values()):
     st.subheader("Selected Recipes")
     recipes = get_recipes_based_on_foods(selected_foods)
 
-    # Afficher les recettes dans une grille
     col1, col2, col3 = st.columns(3)
     for idx, recipe in enumerate(recipes):
         if idx % 3 == 0:
